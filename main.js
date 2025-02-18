225a5a286a9b082233538ac77b45f1b09182b384
ramkamineni <ramkamineni1212@gmail.com>
2025-02-18 23:51:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.write("link chuste challu click cheyadame...");
-  res.end(".....edina pani chusuko raa");
+  res.end(".....");
 });
 server.listen("3101",() => {
   console.log("");
