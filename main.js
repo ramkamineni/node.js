8707b4a2968a32c023f7a0dec376f8488bd8cb66
ramkamineni <ramkamineni1212@gmail.com>
2025-02-18 23:44:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const http = require("http");
 // console.log(http)
 
 const server = http.createServer((req, res) => {
-  res.write("hello world");
-  res.end(" karthik loverboy");
+  res.write("link chuste challu click cheyadame...");
+  res.end(".....edina pani chusuko raa");
 });
 server.listen("3101",() => {
-  console.log("server is running");
+  console.log("");
 });
